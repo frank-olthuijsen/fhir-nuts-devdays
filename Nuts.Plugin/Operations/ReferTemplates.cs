9bcbafd577fae7c395795abd2c8bd03a2dc8795a
Frank Olthuijsen <frank@silva-it.nl>
2023-05-31 12:39:56 +0200

    Cleanup

--- Nuts.Plugin/Operations/ReferTemplates.cs
+++ Nuts.Plugin/Operations/ReferTemplates.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Nuts.Plugin.Operations
+﻿namespace Nuts.Plugin.Operations
 {
     internal static class ReferTemplates
     {
